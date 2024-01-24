d3be168a784e20de1d565008c5d44f098dc9c56a
eliot-christon <eliot.christon.1@etu.sorbonne-universite.fr>
2024-01-24 15:45:05 +0100

    clean lidar nav

--- planning_bolide/src_nav/nav_lidar.py
+++ planning_bolide/src_nav/nav_lidar.py
@@ -16,74 +16,94 @@ from std_msgs.msg import Bool
 
 #%% FUNCTIONS
 
-def nav_3_dials(dist_left:float, dist_center:float, dist_right:float, Kspeed:float, Kdir:float) -> tuple:
+
+def get_dials_ranges(lidar_data:LaserScan, n_dials:int=3, proportion:list=None) -> np.ndarray:
+    if not(proportion is None) and len(proportion) != n_dials:
+        rospy.logwarn("The proportion list must have the same length as the number of dials., Here {} != {}".format(len(proportion), n_dials))
+        proportion = None
+    if len(lidar_data.ranges) < n_dials:
+        rospy.logwarn("The number of dials is greater than the number of points in the lidar data. Here {} > {}".format(n_dials, len(lidar_data.ranges)))
+        return None
+
+    if proportion is None:
+        proportion = [1] * n_dials
+
+    # get the indices of the dials using the proportion
+    # first scale the proportion to sum to the number of points
+    # normalize the proportion
+    proportion = len(lidar_data.ranges) * (np.array(proportion) / sum(proportion))
+    # then round the proportion to get the number of points per dial
+    proportion = np.round(proportion).astype(int)
+
+    # get the indices of the dials
+    dials_indices = np.cumsum(proportion)[:-1]
+
+    # add 0 and the last index to the indices
+    dials_indices = np.insert(dials_indices, 0, 0)
+    dials_indices = np.append(dials_indices, len(lidar_data.ranges))
+
+    print("dials_indices:", dials_indices)
+
+    # get the ranges of the dials
+    dials = []
+    for i in range(n_dials):
+        dials.append(lidar_data.ranges[dials_indices[i]:dials_indices[i+1]])
+
+    return np.array(dials)
+    
+
+
+def crop_cmd_vel(speed:float, direction:float, speed_lim:dict={"min":-1, "max":1}, direction_lim:dict={"min":-1, "max":1}) -> tuple:
+    """Crop the speed and direction commands to the extremum values."""
+    # Crop the speed command
+    if speed > speed_lim["max"]:
+        speed = speed_lim["max"]
+    elif speed < speed_lim["min"]:
+        speed = speed_lim["min"]
+
+    # Crop the direction command
+    if direction > direction_lim["max"]:
+        direction = direction_lim["max"]
+    elif direction < direction_lim["min"]:
+        direction = direction_lim["min"]
+
+    return speed, direction
+
+def nav_3_dials(lidar_data:LaserScan, Kspeed:float, Kdir:float, Karg:float) -> tuple:
     """Return the speed and direction of the robot based on 3 dials range data."""
+
+    # Get the ranges of the dials
+    range_left, _, range_center, _, range_right = get_dials_ranges(lidar_data, n_dials=5, proportion=[7, 1, 2, 1, 7])
+
+    # Compute the mean distance of each dial
+    dist_left = np.mean(range_left)
+    dist_center = np.mean(range_center)
+    dist_right = np.mean(range_right)
+
+    # Argmax step
+    arg = (np.argmax(lidar_data.ranges) - 90)/90
+
     # Compute the speed command
     speed_cmd = Kspeed * dist_center
 
-    # Limit the speed command
-    if speed_cmd > 1:
-        speed_cmd = 1
-    elif speed_cmd < 0.2:
-        speed_cmd = 0.2
-
     # Compute the direction command
-    dir_cmd = - Kdir * (dist_right - dist_left) / speed_cmd
+    dir_cmd = - Kdir * (dist_right - dist_left) / speed_cmd + Karg * arg
 
-    # Limit the direction command
-    if dir_cmd > 1:
-        dir_cmd = 1
-    elif dir_cmd < -1:
-        dir_cmd = -1
-
-    return speed_cmd, dir_cmd
-
+    # Crop the commands
+    return crop_cmd_vel(speed_cmd, dir_cmd, speed_lim={"min":0.2, "max":1})
 
 
 
 #%% CLASS
 class NavLidar():
     def __init__(self):
+        print("NavLidar init")
         self.pub = rospy.Publisher("cmd_vel",SpeedDirection,queue_size=1)
         self.cmd = SpeedDirection()
-
-
-    def Nav(self, Lidar_data, methode = 1):
-
-        i1 = len(Lidar_data)//3
-
-        i2 = 2 * len(Lidar_data)//3
-
-        rospy.Subscriber("/param_change_alert", Bool, self.get_gain)
-
-        self.get_gain()
-
-        gauche = np.average(Lidar_data[0:70])
-        centre = np.average(Lidar_data[80:100])
-        droite = np.average(Lidar_data[110:180])
-
-        arg = (np.argmax(Lidar_data) - 90)/90
-        
-        rospy.loginfo(f"gauche : {gauche}, centre : {centre}, droite : {droite}")
-
-        cmd_vel = self.Kv*centre
-
-        if cmd_vel>1: cmd_vel = 1
-        if ( 0 < cmd_vel < 0.2): cmd_vel = 0.2
-        if cmd_vel<-1:cmd_vel = -1
-        
-        cmd_dir = - self.Kd*(droite-gauche) / cmd_vel + arg * self.Ka
-        
-        if cmd_dir>1: cmd_dir = 1
-        if cmd_dir<-1:cmd_dir = -1
-
-        return cmd_vel,cmd_dir
     
     def get_scan(self, msg:LaserScan):
-        Lidar_data = msg.ranges
-        self.cmd.speed,self.cmd.direction = self.Nav(Lidar_data)
+        self.cmd.speed,self.cmd.direction = nav_3_dials(msg, self.Kv, self.Kd, self.Ka)
         self.pub.publish(self.cmd)
-
 
     def get_gain(self, value = True):
         self.Kd = rospy.get_param("/gain_direction", default = 0.8)
