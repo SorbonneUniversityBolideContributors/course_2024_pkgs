d5dc2760f0442c3e65aecfa9df85b52c018fb99e
eliot-christon <eliot.christon.1@etu.sorbonne-universite.fr>
2024-01-25 15:42:49 +0100

    saving old working nav

--- planning_bolide/src_nav/nav_module/nav_functions.py
+++ planning_bolide/src_nav/nav_module/nav_functions.py
@@ -37,7 +37,6 @@ def get_dials_ranges(lidar_data:LaserScan, n_dials:int=3, proportion:list=None) 
         dials.append(lidar_data.ranges[dials_indices[i]:dials_indices[i+1]])
 
     return np.array(dials)
-    
 
 
 def crop_cmd_vel(
@@ -64,32 +63,34 @@ def crop_cmd_vel(
 
     return SpeedDirection(speed, direction)
 
-# def nav_3_dials(lidar_data:LaserScan, Kspeed:float, Kdir:float, Karg:float, Mode:int) -> SpeedDirection:
-#     """Return the speed and direction of the robot based on 3 dials range data."""
 
-#     # Get the ranges of the dials
-#     range_left, range_right = get_dials_ranges(lidar_data, n_dials=2)
-#     _, range_center, _ = get_dials_ranges(lidar_data, n_dials=3, proportion=[1, 0.5, 1])
+def nav_3_dials_spaced(lidar_data:LaserScan, Kspeed:float, Kdir:float, Karg:float, Mode:int) -> SpeedDirection:
+    """Return the speed and direction of the robot based on 3 dials range data."""
 
-#     # Compute the mean distance of each dial
-#     dist_left = np.mean(range_left)
-#     dist_center = np.mean(range_center)
-#     dist_right = np.mean(range_right)
+    # Get the ranges of the dials
+    range_left, _, range_center, _, range_right = get_dials_ranges(lidar_data, n_dials=5, proportion=[70, 10, 20, 10, 70])
 
-#     # Argmax step
-#     mid_i = len(lidar_data.ranges)//2
-#     arg = (np.argmax(lidar_data.ranges) - mid_i)/mid_i
+    # Compute the mean distance of each dial
+    dist_left = np.mean(range_left)
+    dist_center = np.mean(range_center)
+    dist_right = np.mean(range_right)
 
-#     # Compute the speed command
-#     speed_cmd = Kspeed * dist_center
+    # Argmax step
+    mid_i = len(lidar_data.ranges)//2
+    arg = (np.argmax(lidar_data.ranges) - mid_i)/mid_i
 
-#     # Compute the direction command
-#     dir_cmd = - Kdir * (dist_right - dist_left) / speed_cmd + Karg * arg
+    # Compute the speed command
+    speed_cmd = Kspeed * dist_center
 
-#     cmd_vel = SpeedDirection(speed_cmd, dir_cmd)
+    # Compute the direction command
+    dir_cmd = - Kdir * (dist_right - dist_left) / speed_cmd + Karg * arg
 
-#     # Crop the commands
-#     return crop_cmd_vel(cmd_vel, speed_lim={"min":0.2, "max":1})
+    cmd_vel = SpeedDirection(speed_cmd, dir_cmd)
+
+    # Crop the commands
+    return crop_cmd_vel(cmd_vel, speed_lim={"min":0.2, "max":1})
+
+
 
 def nav_3_dials(lidar_data:LaserScan, Kspeed:float, Kdir:float, Karg:float, Mode:str, FrontRatio: float = 0.2 ) -> SpeedDirection:
     """Return the speed and direction of the robot based on 3 dials range data."""
@@ -126,6 +127,7 @@ def nav_3_dials(lidar_data:LaserScan, Kspeed:float, Kdir:float, Karg:float, Mode
 
     # Crop the commands
     return crop_cmd_vel(cmd_vel, speed_lim={"min":0.2, "max":1})
+
 
 def nav_n_dials(lidar_data:LaserScan, Kspeed:float, Kdir:float, Karg:float, n_dials:int, Mode:str, FrontRatio:float = 0.2) -> SpeedDirection:
     """Return the speed and direction of the robot based on 3 dials range data."""
