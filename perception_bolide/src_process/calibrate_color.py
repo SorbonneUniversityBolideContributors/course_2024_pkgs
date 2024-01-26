16758ff2615feff04a63ecdd8e0f4230e88cbe7f
EndreaMagni <134390660+EndreaMagni@users.noreply.github.com>
2024-01-26 14:36:47 +0100

    Update calibrate_color.py

--- perception_bolide/src_process/calibrate_color.py
+++ perception_bolide/src_process/calibrate_color.py
@@ -58,5 +58,5 @@ class DetectColor:
         
         self.subscriber.unregister()
 
-rospy.init_node("node_test", anonymous = True)
+rospy.init_node("calibration_color", anonymous = False)
 detect_color = DetectColor()
