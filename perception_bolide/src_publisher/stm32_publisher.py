0340f1c67eeb49eaf21fd4109fecef8eaa76b788
Nicolas Hammje <107423377+nicohmje@users.noreply.github.com>
2024-03-06 00:56:14 +0100

    Fix odometry jumping to 0
    Just an oversight in the original code that had the message initialize to 0 every time, but wouldn't return if the CRC failed. Here it instead sends the old values. 
    
    Another implementation could be to forgo sending these at all, but fixed rates algorithms could see failures.

--- perception_bolide/src_publisher/stm32_publisher.py
+++ perception_bolide/src_publisher/stm32_publisher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-__author__ = "Quentin Rolland"
-__email__ = "quentin.rolland@ensea.fr"
+__author__ = "Nicolas Hammje"
+__email__ = "me@nicolashammje.fr"
 __status__ = "Tested"
 __version__ = "1.0.0"
 
@@ -12,57 +12,65 @@ from std_msgs.msg import Float32MultiArray
 
 
 
-# Definitely not the most efficient way of doing this, but we're transmitting 16 bytes so it's OK.
-# Ideally we'd do all this in Cpp, but I don't think there is a spidev equivalent in Cpp, and it's 
-# not like we're being limited by this script. 
-def crc32mpeg2(buf, crc=0xffffffff):
-    for val in buf:
-        crc ^= val << 24
-        for _ in range(8):
-            crc = crc << 1 if (crc & 0x80000000) == 0 else (crc << 1) ^ 0x104c11db7
-    return crc
+class STM32_Parser:
+    def __init__(self):
+        self.BAUDRATE = 100000
+        
+        bus=0 
+        device=1
 
-    
-
-def receiveSensorData():
-    data = spi.xfer2([0x45]*16)  # Envoyer 16 bytes de données nulles et recevoir 16 bytes de données en retour
-
-    vbat = 0
-    yaw = 0
-    ir_gauche = 0
-    ir_droit = 0
-    speed = 0
-    distance_US = 0
+        self.sensor_pub = rospy.Publisher('stm32_sensors', Float32MultiArray, queue_size=10)
+        self.spi = spidev.SpiDev()
+        self.spi.open(bus,device)
+        self.spi.mode = 0
+        self.spi.max_speed_hz = self.BAUDRATE 
 
 
-    if (not crc32mpeg2(data)):
-        vbat = (data[0] << 8) | data[1]
-        yaw = (data[2] << 8) | data[3]
-        ir_gauche = (data[4] << 8) | data[5]
-        ir_droit = (data[6] << 8) | data[7]
-        speed = (data[8] << 8) | data[9]
-        distance_US = (data[10] << 8) | data[11]
-    
+        self.vbat = 0
+        self.yaw = 0
+        self.ir_gauche = 0
+        self.ir_droit = 0
+        self.speed = 0
+        self.distance_US = 0
 
-    sensor_data.data = [yaw, speed, ir_gauche, ir_droit, distance_US]
-    pub.publish(sensor_data)
+        self.sensor_data = Float32MultiArray() # table for the sensors data
+
+
+
+    # Definitely not the most efficient way of doing this, but we're transmitting 16 bytes so it's OK.
+    # Ideally we'd do all this in Cpp, but I don't think there is a spidev equivalent in Cpp, and it's 
+    # not like we're being limited by this script. 
+    def crc32mpeg2(self,buf, crc=0xffffffff):
+        for val in buf:
+            crc ^= val << 24
+            for _ in range(8):
+                crc = crc << 1 if (crc & 0x80000000) == 0 else (crc << 1) ^ 0x104c11db7
+        return crc
+
+        
+
+    def receiveSensorData(self):
+        data = self.spi.xfer2([0x45]*16)  # SPI happens simultaneously, so we need to send to receive. 
+
+
+        if (not self.crc32mpeg2(data)):
+            self.vbat = (data[0] << 8) | data[1]
+            self.yaw = (data[2] << 8) | data[3]
+            self.ir_gauche = (data[4] << 8) | data[5]
+            self.ir_droit = (data[6] << 8) | data[7]
+            self.speed = (data[8] << 8) | data[9]
+            self.distance_US = (data[10] << 8) | data[11]
+
+        print(self.vbat)
+
+        self.sensor_data.data = [self.yaw, self.speed, self.ir_gauche, self.ir_droit, self.distance_US]
+        self.sensor_pub.publish(self.sensor_data)
         
 if __name__ == '__main__':
-    # variables
-    sensor_data = Float32MultiArray() # table for the sensors data
 
-    bus=0 
-    device=1
-
-    spi = spidev.SpiDev()
-    spi.open(bus,device)
-    spi.mode = 0
-    spi.max_speed_hz = 100000  # Définir la vitesse maximale du bus SPI à 100 kHz
-
-    # Create a STM32DataReceiver and start it
+    Parser = STM32_Parser()    
     rospy.init_node('stm32_publisher')
-    pub = rospy.Publisher('stm32_sensors', Float32MultiArray, queue_size=10)
     rate = rospy.Rate(150)
     while(not rospy.is_shutdown()):
-        receiveSensorData()
+        Parser.receiveSensorData()
         rate.sleep()
